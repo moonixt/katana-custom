ba7201ec65ab0c284fd272822a622defc76383ef
arch <derekoob@hotmail.com>
2025-09-01 23:55:36 -0300

    Update banner text and improve display message in showBanner function

--- internal/runner/banner.go
+++ internal/runner/banner.go
@@ -6,18 +6,19 @@ import (
 )
 
 var banner = (`
-   __        __                
-  / /_____ _/ /____ ____  ___ _
- /  '_/ _  / __/ _  / _ \/ _  /
-/_/\_\\_,_/\__/\_,_/_//_/\_,_/							 
-`)
+   __                                           
+  _____ _____  ______ ______   ___________ 
+ /     \\__  \ \____ \\____ \_/ __ \_  __ \
+|  Y Y  \/ __ \|  |_> >  |_> >  ___/|  | \/
+|__|_|  (____  /   __/|   __/ \___  >__|   
+      \/     \/|__|   |__|        \/       `)
 
 var version = "v1.2.2"
 
 // showBanner is used to show the banner to the user
 func showBanner() {
 	gologger.Print().Msgf("%s\n", banner)
-	gologger.Print().Msgf("\t\tprojectdiscovery.io\n\n")
+	gologger.Print().Msgf("\t\tSEO DUO\n")
 }
 
 // GetUpdateCallback returns a callback function that updates katana
